cbdab0bde3f63c41fb6d06133ed70c554e182f77
Piera Riccio <48887623+PieraRiccio@users.noreply.github.com>
2022-06-16 15:20:21 +0200

    Update spot_errors.py

--- OpenFilter/spot_errors.py
+++ OpenFilter/spot_errors.py
@@ -1,3 +1,20 @@
+"""
+OpenFilter main code
+Copyright (C) 2022  ELLIS Alicante Foundation
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+contact information: piera@ellisalicante.org
+"""
+
 import numpy as np
 import os
 from PIL import Image
